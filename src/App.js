828ff4cb3f609489b586477be89b12885def8e98
Kumilachew-g <kumilachew1212@gmail.com>
2022-10-17 10:27:37 +0300

    Add link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Home from './components/home/Home';
 import Navbar from './components/navigation/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
